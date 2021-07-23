835a462956fe26651076b9f6e27fe643670aa7aa
Dzstar <Dzstar@163.com>
2021-07-23 23:46:41 +0800

    gitee commit

--- src/Studydaydyaup/studydaydayup.java
+++ src/Studydaydyaup/studydaydayup.java
@@ -2,14 +2,12 @@ package Studydaydyaup;
 
 public class studydaydayup {
     public static void main(String[] args) {
-        System.out.println("好好学习，天天向上。");
-        System.out.println("伟大领袖毛主席");
+        System.out.println("好好学习，天天向上。")
         System.out.println("邓导好帅");
         System.out.println("Dd好帅");
         System.out.println("master test!");
         System.out.println("Hot-fix");
-        
-        System.out.println("伟大领袖毛主席");
+
         System.out.println("pull Test!");
         System.out.println("pull Test!");
     }
