3e87ce713fac4f351322b3cf774cbb98e8e59a00
Dzstar <Dzstar@163.com>
2021-07-23 12:22:52 +0800

    third commit

--- src/Studydaydyaup/studydaydayup.java
+++ src/Studydaydyaup/studydaydayup.java
@@ -3,5 +3,6 @@ package Studydaydyaup;
 public class studydaydayup {
     public static void main(String[] args) {
         System.out.println("好好学习，天天向上。");
+        System.out.println("伟大领袖毛主席");
     }
 }
